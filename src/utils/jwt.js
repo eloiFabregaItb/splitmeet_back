eb7dc052bd9a92a8a3f6aedb57dee69dd446e640
encarbassot <elgrefa@gmail.com>
2023-10-19 16:57:18 +0200

    jwt login

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,11 +1,34 @@
 import jwt from "jsonwebtoken";
 
+const JWT_TIMEOUT = process.env.JWT_TIMEOUT || "48h"
+const JWT_SECRET = process.env.JWT_SECRET || "secretJWT"
 
-function jwtSign(userKey,seconds){
-    return jwt.sign({user_key:userKey}, process.env.JWT_SECRET, { expiresIn:`${seconds}s` });
-  }
+export function jwtSign(data){
+    return jwt.sign(data, JWT_SECRET, { expiresIn: JWT_TIMEOUT });
+}
   
 
+
+
+
+
+
+
+
+
+
+/**
+ * Esta funcion 
+ * 
+ 
+EJEMPLO:                 _________
+router.post("/protected",jwtVerify,async(req,res)=>{
+  const {usr_id} = req
+  res.json({success:true,msg:"authentificated",user:req.by})
+})
+
+
+ */
 export function jwtVerify (req, res, next){
   const token =
     req.body.token || req.query.token || req.headers["x-access-token"];
@@ -14,8 +37,9 @@ export function jwtVerify (req, res, next){
     return res.status(403).json({success:false,msg:"A token is required for authentication"});
   }
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
+    const {usr_id,...decoded} = jwt.verify(token, JWT_SECRET);
+    req.by = decoded;
+    req.usr_id = usr_id
   } catch (err) {
     return res.status(401).json({success:false,msg:"Invalid Token"});
   }
