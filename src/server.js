8b46cd9f4bf73ba65b9612e90d93476e33f21b41
orillad <orillad2003@gmail.com>
2023-10-20 21:37:10 +0200

    changes

--- src/server.js
+++ src/server.js
@@ -8,8 +8,6 @@ import { Server } from 'socket.io'
 import { createServer } from 'node:http'
 import logger from 'morgan'
 import os from "os"
-import db from './db/db.js'
-import cors from "cors"
 
 // own modules
 import router_test from "./routes/template.js"
@@ -24,29 +22,11 @@ import passport from 'passport'
 
 // ----------- CONFIG -----------
 
-const NODE_ENV = process.env.NODE_ENV || "production"
-const NODE_ENV_PRODUCTION = process.env.NODE_ENV_PRODUCTION || "production"
-const NODE_ENV_DEVELOPMENT = process.env.NODE_ENV_DEVELOPMENT || "development"
-
-
-export const IS_IN_PRODUCTION = NODE_ENV !== NODE_ENV_DEVELOPMENT
 
 //api
 const PORT = process.env.PORT ?? 3000
 const app = express()
 const server = createServer(app)
-
-
-//DATABASE CONNECTION TESTING
-// const [rows] = await db.query("SELECT * FROM Users")
-// // const [rows] = await db.query("INSERT INTO Users (usr_id,usr_mail,usr_name,usr_password,usr_oauth,usr_img) VALUES (?,?,?,?,?,?);",
-// // ["6c1fbd83-9e4a-45f2-8d84-17f74289eloi","elgrefa@gmail.com","elioputo","miau",0,"default.img"])
-// console.log(rows)
-
-
-
-
-
 
 
 //socket.io
@@ -59,15 +39,12 @@ export const io = new Server(server, {
 
 
 // ----------- MIDLEWARE -----------
-if(!IS_IN_PRODUCTION){
-  app.use(cors({
-    origin: '*'
-  }))
-  app.use(logger('dev'))
-}
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+if (process.env.NODE_ENV === 'development') {
+  app.use(logger('dev'))
+}
 
 app.use(session({
   secret: 'secret_session',
@@ -91,7 +68,7 @@ io.on('connection', socketRecieverManager)
 // ----------- SERVER -----------
 server.listen(PORT, () => {
   // Find the IPv4 addresses for internal network interfaces
-  if(!IS_IN_PRODUCTION){
+  if(process.env.NODE_ENV === 'development'){
     const interfaces = os.networkInterfaces();
     const addresses = Object.values(interfaces)
       .flat()
