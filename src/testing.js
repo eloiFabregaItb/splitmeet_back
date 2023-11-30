66bd927182a594543edbad2e1c62bd90cd624b15
encarbassot <elgrefa@gmail.com>
2023-11-30 15:20:36 +0100

    post info

--- src/testing.js
+++ src/testing.js
@@ -2,14 +2,35 @@ import db from "./db/db.js";
 import { User } from "./models/User.js";
 // import multer from "multer";
 
+//TABLES
+const tables = await db.query("SHOW TABLES");
+console.log(tables[0].map((x) => x.Tables_in_splitmeet).join(" | "));
+
 //DB SELECT
 // let [rows, fields] = await db.query("SELECT * FROM Users");
 // console.log(rows);
 
 //DB SELECT EVENTS
-// let [rows,fields] = await db.query("SELECT * FROM Events")
-// console.log(rows.map(x=>new Event(x)))
-// console.log(rows)
+// let [rows, fields] = await db.query("SELECT * FROM Events");
+// console.log(rows.map((x) => new Event(x)));
+// console.log(rows);
+
+//DB EVENT INFO
+// let [rows, fields] = await db.query("SELECT * FROM Events WHERE evt_url = ?", [
+//   "7CKK1dlQ",
+// ]);
+// const ev = rows[0];
+// console.log(ev);
+// let [rows2] = await db.query("SELECT * FROM Expensses WHERE evt_id = ?", [
+//   ev.evt_id,
+// ]);
+// const exp = rows2[0];
+// console.log(exp);
+// let [tra] = await db.query(
+//   "SELECT * FROM Expensses_transaction WHERE exp_id = ?",
+//   [exp.exp_id]
+// );
+// console.log(tra);
 
 //DB UPDATE
 // await db.query(`UPDATE Events SET evt_modification_timestamp = ${getNowTimestamp()}  WHERE evt_id = 'c86e47e7-0f88-424d-8ccb-2937c0535bc2'`)
@@ -22,12 +43,12 @@ import { User } from "./models/User.js";
 // await db.query(`DELETE FROM User_participation WHERE usr_id = ?`,['1a2b3c4d5e6f7g8h9i0j1k2l3m4n5o6p'])
 
 //JWT OF USER
-const [rows] = await db.query("SELECT * FROM Users WHERE usr_id =  ?", [
-  "1a2b3c4d5e6f7g8h9i0j1k2l3m4n5o6p",
-]);
-const user = new User(rows[0]);
-user.signJWT();
-console.log(user.name, user.jwt);
+// const [rows] = await db.query("SELECT * FROM Users WHERE usr_id =  ?", [
+//   "85d45e67-f9c9-44b0-8946-f9e9f8443c4a",
+// ]);
+// const user = new User(rows[0]);
+// user.signJWT();
+// console.log(user.name, user.jwt);
 
 //MAILER
 
